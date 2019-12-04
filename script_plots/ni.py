9920a6aa15d941dd9c2093228803015c6be03363
bayer <bayer@tropos.de>
2019-12-04 11:19:40 +0100

    loop modified

--- script_plots/ni.py
+++ script_plots/ni.py
@@ -12,9 +12,6 @@ import calendar
 
 """Insert de initial and final dates as strings as 20190107(year:2019/month:01/day:07)"""
 
-
-    
-  
 def statistic(i8date,f8date):
     methodbts = "global" 
     iy=int(i8date[0:4])
@@ -30,7 +27,51 @@ def statistic(i8date,f8date):
     """Define the main path where the images should be saved"""
     image_path="/home/bayer/uv/images/"
 
+    for ys in range(iy,fy+1):
+        for imonth in range(1,13):
+            if ys==iy and imonth<im:
+                continue
+            elif ys==fy and imonth>fm:
+                break
+            else:
+                numberOfDays = calendar.monthrange(ys, imonth)[1]
+                for iday in range(1,numberOfDays+2):
+                    if iday<ida:
+                        iday=iday+1
+                    elif iday==numberOfDays+1 and imonth<=11:
+                        imonth=imonth+1
+                        ida=1
+                    elif iday==numberOfDays+1 and imonth==12:
+                        imonth=1
+                        ida=1
+                        ys=ys+1
+                    elif imonth==fm and ys==fy and iday>fda:
+                        break
+                    elif iday>=ida and iday<=numberOfDays:
+                        path_file=main_path+str(ys)+"/"+str(imonth).zfill(2)+"/"+str(iday).zfill(2)+"/"+"MP"+str(ys-2000).zfill(2)+str(imonth).zfill(2)+str(iday).zfill(2)+".OR0"
+                        if os.path.isfile(path_file):
+                            if os.stat(path_file).st_size<1:
+                                print('file is empty '+path_file)
+                                iday=iday+1
+                            else:
+                                i8date=str(ys)+str(imonth).zfill(2)+str(iday).zfill(2)
+                                #Obtanin the directory data from the OR0 files
+                                d_bts1day=bts.read_oro_bts(path_file,methodbts,i8date)
+                                #Ploting function
+                                plotme.plotme(d_bts1day,i8date,image_path)
+                                iday=iday+1
+                        else:
+                            print(path_file+" does not exist")
+                            iday=iday+1
     
+#####################################################################################                                                            
+statistic('20190302','20190302')
+
+"""files that show error: 
+20180815
+"""
+
+"""
     for iy in range(iy,fy+1):
         if iy!=fy:
             path = main_path + str(iy) + "/"
@@ -43,10 +84,14 @@ def statistic(i8date,f8date):
                             path2 = path1 + str(ida).zfill(2) + "/"
                             path_file=path2+"MP"+str(iy-2000).zfill(2)+str(im).zfill(2)+str(ida).zfill(2)+".OR0"
                             if os.path.isfile(path_file):
-                                i8date=str(iy)+str(im).zfill(2)+str(ida).zfill(2)
-                                d_bts1day=bts.read_oro_bts(path_file,methodbts,i8date)
-                                plotme.plotme(d_bts1day,i8date,image_path)
-                                ida=ida+1
+                                if os.stat(path_file).st_size<1:
+                                    print('file is empty '+path_file)
+                                    ida=ida+1
+                                else:
+                                    i8date=str(iy)+str(im).zfill(2)+str(ida).zfill(2)
+                                    d_bts1day=bts.read_oro_bts(path_file,methodbts,i8date)
+                                    plotme.plotme(d_bts1day,i8date,image_path)
+                                    ida=ida+1
                             else:
                                 ida=ida+1
                                 print(path_file+" does not exist")
@@ -69,10 +114,14 @@ def statistic(i8date,f8date):
                         if ida<=numberOfDays:
                             path_file=path2+"MP"+str(iy-2000).zfill(2)+str(im).zfill(2)+str(ida).zfill(2)+".OR0"
                             if os.path.isfile(path_file):
-                                i8date=str(iy)+str(im).zfill(2)+str(ida).zfill(2)
-                                d_bts1day=bts.read_oro_bts(path_file,methodbts,i8date)
-                                plotme.plotme(d_bts1day,i8date,image_path)
-                                ida=ida+1
+                                if os.stat(path_file).st_size<1:
+                                    print('file is empty '+path_file)
+                                    ida=ida+1
+                                else:
+                                    i8date=str(iy)+str(im).zfill(2)+str(ida).zfill(2)
+                                    d_bts1day=bts.read_oro_bts(path_file,methodbts,i8date)
+                                    plotme.plotme(d_bts1day,i8date,image_path)
+                                    ida=ida+1
                             else:
                                 ida=ida+1
                                 print(path_file+" does not exist")
@@ -86,27 +135,19 @@ def statistic(i8date,f8date):
                         path2= path1 + str(ida).zfill(2) + "/"
                         path_file=path2+"MP"+str(iy-2000).zfill(2)+str(im).zfill(2)+str(ida).zfill(2)+".OR0"
                         if os.path.isfile(path_file):
-                            i8date=str(iy)+str(im).zfill(2)+str(ida).zfill(2)
-                            d_bts1day=bts.read_oro_bts(path_file,methodbts,i8date)
-                            plotme.plotme(d_bts1day,i8date,image_path)
-                            ida=ida+1
+                            if os.stat(path_file).st_size<1:
+                                print('file is empty '+path_file)
+                                ida=ida+1
+                            else:
+                                i8date=str(iy)+str(im).zfill(2)+str(ida).zfill(2)
+                                d_bts1day=bts.read_oro_bts(path_file,methodbts,i8date)
+                                plotme.plotme(d_bts1day,i8date,image_path)
+                                ida=ida+1
                         else:
                             ida=ida+1 
                             print(path_file+" does not exist")
-                                                            
-statistic('20190227','20190302')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""                            
+                            
+                            
+                            
+                            
