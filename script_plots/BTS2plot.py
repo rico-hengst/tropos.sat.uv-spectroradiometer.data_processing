a2533757f1797ed6edfc91f85b9073aa239e7b9c
bayer <bayer@tropos.de>
2020-01-16 09:48:40 +0100

    changed name for the plotting function

--- script_plots/BTS2plot.py
+++ script_plots/BTS2plot.py
@@ -6,9 +6,9 @@ Created on Tue Nov 19 13:13:35 2019
 @author: bayer
 """
 import matplotlib.pyplot as plt
-from matplotlib import colors
-import matplotlib as mpl
-import numpy as np
+# from matplotlib import colors
+# import matplotlib as mpl
+# import numpy as np
 
 
 def plotme(d_bts1day, i8date, image_path):
@@ -63,53 +63,3 @@ def plotme(d_bts1day, i8date, image_path):
     """save the plot as pdf file """
     plot_name = image_path + i8date + '.pdf'
     fig.savefig(plot_name)
-#    cmap = mpl.colors.ListedColormap(['green', 'yellow', 'orange', 'red'])                                  
-#    cmap.set_over('violet')
-#    cmap.set_under('green')
-#    bounds = [1, 3, 6, 8, 10]
-#    ax4.norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
-#    ax4.cb3 = mpl.colorbar.ColorbarBase(ax, cmap=cmap, boundaries=[-10] + bounds + [10],
-#    extend='both', extendfrac='auto', ticks=bounds, spacing='uniform', orientation='horizontal')
-#    ax4.cb3.set_label('Custom extension lengths, some other units')
-
-#    horizontalbar_chart = pygal.HorizontalBar()
-#    horizontalbar_chart.add('Low ', 3)
-#    horizontalbar_chart.add('Moderate ', 6)
-#    horizontalbar_chart.add('High ', 8)
-#    horizontalbar_chart.add('Very High ', 11)
-#    horizontalbar_chart.render()    
-#    norm = colors.Normalize(uvmin=0, uvmax=11)
-#    im = ax3.imshow(np.arange(11)
-    #add_colorbar(im)
-    #plt.colorbar(im,orientation='horizontal', fraction=0.046, pad=0.04)
-    
-#fig, ax = plt.subplots(figsize=(4, 1))
-#fig.subplots_adjust(bottom=0.6)
-#
-#cmap = mpl.cm.cool
-#norm = mpl.colors.Normalize(vmin=0, vmax=11)
-#
-#cb1 = mpl.colorbar.ColorbarBase(ax, cmap=cmap, norm=norm, orientation='horizontal')
-#cb1.set_label('UV Index')
-#fig.show()
-#
-#ax.set_ticklabels(['low', 'medium', 'high'])
-#
-#fig, ax = plt.subplots(figsize=(10, 1))
-#fig.subplots_adjust(bottom=0.5)
-#
-#cmap = mpl.colors.ListedColormap(['green', 'yellow', 'orange', 'red'])
-#bounds = [0,3,6,8,11]
-#norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
-#cb2 = mpl.colorbar.ColorbarBase(ax, cmap=cmap,
-#                                norm=norm,
-#                                ticks=bounds,
-#                                spacing='proportional',
-#                                orientation='horizontal')
-#cb2.set_label('UV Index')
-#fig.show()
-# 
-#    ax3.cbar = fig.colorbar(d_bts1day["uvind"],  shrink=0.95)
-#    ax3.cbar.set_ticks(np.arange(0, 1.1, 0.5))
-#    ax3.cbar.set_ticklabels(['low', 'moderate', 'high', 'very high'])
-#    fig.colorbar( ax=ax4, orientation='horizontal', fraction=.1)
