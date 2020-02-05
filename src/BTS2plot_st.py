5e20d838f802b9392bb6cb896c58abe3a07394e9
bayer <bayer@tropos.de>
2020-02-05 14:55:31 +0100

    modification for saving statistic graphic

--- src/BTS2plot_st.py
+++ src/BTS2plot_st.py
@@ -11,9 +11,12 @@ import matplotlib.pyplot as plt
 
 
 
-def plot_st(missing_days, s, f):
+def plot_st(missing_days, s, f, image_path):
     print(range(len(missing_days)))
     plt.bar(range(len(missing_days)),list(missing_days.values()))
     plt.xticks(range(len(missing_days)),list(missing_days.keys()),rotation='vertical')
     plt.title('Missing days: '+s +'-'+f)
+    """save the plot as pdf file """
+    plot_name =image_path+'Missing days: '+s +'-'+f+ '.pdf'
+    plt.savefig(plot_name)
     plt.show()
