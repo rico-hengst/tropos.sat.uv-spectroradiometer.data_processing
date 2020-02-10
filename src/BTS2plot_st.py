18f48ba65afe12fc2d2038546b49e2cbc3a9420e
bayer <bayer@tropos.de>
2020-02-10 10:07:41 +0100

    change saving

--- src/BTS2plot_st.py
+++ src/BTS2plot_st.py
@@ -8,15 +8,19 @@ Created on Tue Feb  4 14:15:43 2020
 
 
 import matplotlib.pyplot as plt
-
+from matplotlib.widgets import Slider, Button, RadioButtons
 
 
 def plot_st(missing_days, s, f, image_path):
-    print(range(len(missing_days)))
-    plt.bar(range(len(missing_days)),list(missing_days.values()))
+    plt.bar(range(len(missing_days)),list(missing_days.values()),linewidth=2.0)
     plt.xticks(range(len(missing_days)),list(missing_days.keys()),rotation='vertical')
-    plt.title('Missing days: '+s +'-'+f)
+    plt.title('Missing Days: '+s +'-'+f)
+    
+    
+    
     """save the plot as pdf file """
-    plot_name =image_path+'Missing days: '+s +'-'+f+ '.pdf'
+    plot_name =image_path+'MissingDays_'+s +'-'+f+ '.pdf'
     plt.savefig(plot_name)
     plt.show()
+    
+#,top=0.966, bottom=0.054, left=0.024, right=0.992, hspace=0.0, wspace=0.0    
