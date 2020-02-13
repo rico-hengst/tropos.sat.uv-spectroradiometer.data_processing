d25759127b26bed9a2a90ce1808830fa52bb9eff
bayer <bayer@tropos.de>
2020-02-13 09:50:53 +0100

    modified st function for 1 plot a month

--- src/BTS2plot_st.py
+++ src/BTS2plot_st.py
@@ -12,15 +12,12 @@ from matplotlib.widgets import Slider, Button, RadioButtons
 
 
 def plot_st(missing_days, s, f, image_path):
+    if f==00: f=12
     plt.bar(range(len(missing_days)),list(missing_days.values()),linewidth=2.0)
     plt.xticks(range(len(missing_days)),list(missing_days.keys()),rotation='vertical')
-    plt.title('Missing Days: '+s +'-'+f)
-    
-    
-    
+    plt.ylim((0,1))
+    plt.title('Missing Days: '+str(s) +'/'+str(f).zfill(2))
     """save the plot as pdf file """
-    plot_name =image_path+'MissingDays_'+s +'-'+f+ '.pdf'
+    plot_name =image_path+'MissingDays_'+str(s) +'-'+str(f).zfill(2)+ '.pdf'
     plt.savefig(plot_name)
     plt.show()
-    
-#,top=0.966, bottom=0.054, left=0.024, right=0.992, hspace=0.0, wspace=0.0    
