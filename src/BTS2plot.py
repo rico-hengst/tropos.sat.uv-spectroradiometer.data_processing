75a9509fe0eef81bc81a4dd9dfd272b1575b7e1f
bayer <bayer@tropos.de>
2020-06-16 13:21:17 +0200

    adding key to chose which timezone should be plotted

--- src/BTS2plot.py
+++ src/BTS2plot.py
@@ -9,14 +9,19 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import host_subplot
 import mpl_toolkits.axisartist as AA
 import pandas as pd
-
+import pytz
+import numpy as np
 # units, see also https://www.uni-kiel.de/med-klimatologie/uvinfo.html
 # https://www.bundesfachverband-besonnung.de/fileadmin/download/solaria2005/Solarium_Sonne.pdf
 
-def plotme(d_bts1day, day, config):    
+def plotme(d_bts1day, day, config):
+  
+    """creating plot title"""
     plt.suptitle(config.get('STATION','station_name') + ' ('+ config.get('STATION','station_lat') + ' '+\
                  config.get('STATION','station_lon') + ')\nDate: '+day.strftime('%Y')+ '/' + day.strftime('%m')+ \
                  '/' + day.strftime('%d'))
+        
+    """creating the 3 y axes for the ploting"""
     host = host_subplot(111, axes_class=AA.Axes)
     plt.subplots_adjust(right=0.75)
     par1 = host.twinx()
@@ -33,14 +38,33 @@ def plotme(d_bts1day, day, config):
     host.set_ylabel("UV-Index")
     par1.set_ylabel("UV-B Irradiance [$W/m^2$]")
     par2.set_ylabel("UV-A Irradiance [$W/m^2$]")
-
     """adding the varibles for plotting and setting the colors and labels"""
-    p1, = host.plot(d_bts1day["time"],d_bts1day["uvind"],"k-",linestyle=':', label="UV-Index")
-    p2, = par1.plot(d_bts1day["time"],d_bts1day["uvb"]*10, "r-", label="UV-B")
-    p3, = par2.plot(d_bts1day["time"],d_bts1day["uva"], "b-", label="UV-A")
     
-    """defining the limits of the axes"""
-    host.set_xlim(4, 21)
+    
+#    if config.get('TIMEZONE','local_time') in config:    
+    """define local timezone from the config file"""
+    new_timezone = pytz.timezone(config.get('TIMEZONE','local_time', fallback='UTC'))
+    
+    """changing time for local time"""
+    for x in range(0,len(d_bts1day['datetime'])):
+        a=pd.Timestamp(d_bts1day['datetime'][x])
+        a=pytz.UTC.localize(a)
+        d_bts1day['datetime'][x]=float(a.astimezone(new_timezone).strftime("%H%M"))/100
+    x=np.array(d_bts1day['datetime'])
+    x=x.astype('float64') 
+    host.set_xlabel("Time ("+str(new_timezone)+")")
+    # else:
+    #     x=np.array(d_bts1day['time'])
+    #     x=x.astype('float64')
+    #     host.set_xlabel("Time (UTC)")
+        
+        
+    p1, = host.plot(x,d_bts1day["uvind"],"k-",linestyle=':', label="UV-Index")
+    p2, = par1.plot(x,d_bts1day["uvb"], "r-", label="UV-B")
+    p3, = par2.plot(x,d_bts1day["uva"], "b-", label="UV-A")
+    
+    """defining the limits of the axes"""  #preguntar como hacer cn los limites
+    host.set_xlim(4, 22)
     host.set_ylim(0, 13)
     par1.set_ylim(0, 20)
     par2.set_ylim(0, 70)
@@ -55,15 +79,15 @@ def plotme(d_bts1day, day, config):
     par1.axis["right"].label.set_color(p2.get_color())
     
     """full filling the curve under the UV-Index curve"""
-    plt.fill_between(d_bts1day["time"], d_bts1day["uvind"],  where=d_bts1day["uvind"]<2, 
+    plt.fill_between(x, d_bts1day["uvind"],  where=d_bts1day["uvind"]<2, 
                         facecolor='green', alpha='0.5', interpolate=True)
-    plt.fill_between(d_bts1day["time"], d_bts1day["uvind"],  where=d_bts1day["uvind"] >2 , 
+    plt.fill_between(x, d_bts1day["uvind"],  where=d_bts1day["uvind"] >2 , 
                         facecolor='yellow', alpha='0.5', interpolate=True)
-    plt.fill_between(d_bts1day["time"], d_bts1day["uvind"],  where=d_bts1day["uvind"] >5 , 
+    plt.fill_between(x, d_bts1day["uvind"],  where=d_bts1day["uvind"] >5 , 
                         facecolor='orange', alpha='0.5', interpolate=True)
-    plt.fill_between(d_bts1day["time"], d_bts1day["uvind"],  where=d_bts1day["uvind"] >7 , 
+    plt.fill_between(x, d_bts1day["uvind"],  where=d_bts1day["uvind"] >7 , 
                         facecolor='red', alpha='0.5', interpolate=True)
-    plt.fill_between(d_bts1day["time"], d_bts1day["uvind"],  where=d_bts1day["uvind"]>10, 
+    plt.fill_between(x, d_bts1day["uvind"],  where=d_bts1day["uvind"]>10, 
                         facecolor='purple', alpha='0.5', interpolate=True)
     
     """adding footnote"""
