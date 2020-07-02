ec76a899963bd62453578260fbc319ceeac319e9
Rico Hengst <rico.hengst@tropos.de>
2020-07-02 09:08:25 +0000

    Improve plotting data * change UVindex plot, now chart bar is used * fixed bug of used time axis variable x, now x_dict is used instead * set xlim in dependency of the used time_zone * add colorbar of UVindex * homogenize alignment of all different y-axis labels * colorize yaxis and thicks of UVA & UVB

--- src/BTS2plot.py
+++ src/BTS2plot.py
@@ -12,22 +12,31 @@ import pandas as pd
 import pytz
 import numpy as np
 from matplotlib.ticker import FixedLocator
+import matplotlib.cm as cm
+from matplotlib.colors import Normalize
+from matplotlib.cm import ScalarMappable
 # units, see also https://www.uni-kiel.de/med-klimatologie/uvinfo.html
 # https://www.bundesfachverband-besonnung.de/fileadmin/download/solaria2005/Solarium_Sonne.pdf
 
 def plotme(d_bts1day, day, config):
+    
+    """get wished timezone from the config file"""
+    new_timezone = pytz.timezone(config.get('TIMEZONE','local_time', fallback='UTC'))
+    
+    fig, ax = plt.subplots()
+    ax.axis('off')
   
     """creating plot title"""
-    plt.suptitle(config.get('STATION','station_name') + ' ('+ config.get('STATION','station_lat') + ' '+\
-                 config.get('STATION','station_lon') + ')\nDate: '+day.strftime('%Y')+ '/' + day.strftime('%m')+ \
-                 '/' + day.strftime('%d'))
+    plt.suptitle('Station: ' + config.get('STATION','station_name') + \
+    '\nDate/Timezone: '+day.strftime('%Y-%m-%d')+ \
+    ' / ' + str(new_timezone))
         
     """creating the 3 y axes for the ploting"""
     host = host_subplot(111, axes_class=AA.Axes)
     plt.subplots_adjust(right=0.75)
     par1 = host.twinx()
     par2 = host.twinx()
-    offset = 60
+    offset = 50
     new_fixed_axis = par2.get_grid_helper().new_fixed_axis
     par2.axis["right"] = new_fixed_axis(loc="right", axes=par2,
                                         offset=(offset, 0))
@@ -35,68 +44,82 @@ def plotme(d_bts1day, day, config):
     par2.axis["right"].toggle(all=True)
     
     """Label the axes"""
-    host.set_xlabel("Time (UTC)")
     host.set_ylabel("UV-Index")
-    par1.set_ylabel("UV-B Irradiance [$W/m^2$]")
-    par2.set_ylabel("UV-A Irradiance [$W/m^2$]")
+    host.set_xlabel("Time [$hour$]")
+    par1.set_ylabel("UV-A Irradiance [$W/m^2$]")
+    par2.set_ylabel("UV-B Irradiance [$W/m^2$]")
     """adding the varibles for plotting and setting the colors and labels"""
     
-    """define local timezone from the config file"""
-    new_timezone = pytz.timezone(config.get('TIMEZONE','local_time', fallback='UTC'))
     
     """changing time for local time"""
-    x=np.array(d_bts1day['datetime'])
+    x_dict = {"datetime_new":[],"hour_decimal":[]}
     for z in range(0,len(d_bts1day['datetime'])):
-        a=pd.Timestamp(d_bts1day['datetime'][z])
-        a=pytz.UTC.localize(a)
-        x[z]=float(a.astimezone(new_timezone).strftime("%H%M"))/100
-    x=x.astype('float64') 
-    host.set_xlabel("Time ("+str(new_timezone)+")")
+        x_dict["datetime_new"].append( d_bts1day['datetime'][z].astimezone(new_timezone) );
+        x_dict["hour_decimal"].append( x_dict["datetime_new"][-1].hour + x_dict["datetime_new"][-1].minute/60 + x_dict["datetime_new"][-1].second/3600)
+   
     
-    #p1, = host.plot(x,d_bts1day["uvind"],"k-",linestyle=':', label="UV-Index")
-    p2, = par1.plot(x,d_bts1day["uvb"], "r-", label="UV-B")
-    p3, = par2.plot(x,d_bts1day["uva"], "b-", label="UV-A")
+    #p0, = host.plot(x,d_bts1day["uvind"],"k-",linestyle=':', label="UV-Index")
+    p1, = par1.plot(x_dict["hour_decimal"],d_bts1day["uva"], "b-", label="UV-A")
+    p2, = par2.plot(x_dict["hour_decimal"],d_bts1day["uvb"], "r-", label="UV-B")
+    
     
     """defining the limits of the axes"""  #preguntar como hacer cn los limites
-    host.set_xlim(4, 22)
-    host.set_ylim(0, 13)
+    utcoffset = x_dict["datetime_new"][1].utcoffset().total_seconds()/3600
+    host.set_xlim(2+utcoffset, 20+utcoffset)
+    host.set_ylim(0, 10)
+    par1.yaxis.set_major_locator(FixedLocator(np.arange(0, 10, 2)))
 
-    par1.set_ylim(0, 20)
-    par1.yaxis.set_major_locator(FixedLocator(np.arange(0, 20, 3)))
+    par1.set_ylim(0, 70)
+    par1.yaxis.set_major_locator(FixedLocator(np.arange(0, 70.1, 14)))
 
-    par2.set_ylim(0, 70)
+    par2.set_ylim(0, 15)
+    par2.yaxis.set_major_locator(FixedLocator(np.arange(0, 15.1, 3)))
     
     """adding legend and function"""
     host.legend()
     host.grid()
 
+
     """coloring the axis"""
-    par1.spines["right"].set_color("red")
-    par2.spines["right"].set_color("blue")
-
-    par1.axis["right"].label.set_color(p2.get_color())    #coloring the label    
-    par2.axis["right"].label.set_color(p3.get_color())
+    par1.axis["right"].label.set_color(p1.get_color())    #coloring the label    
+    par2.axis["right"].label.set_color(p2.get_color())
     
-    par1.tick_params(axis='y', colors=p2.get_color())     #coloring the ticks
-    par2.tick_params(axis='y', colors=p3.get_color())
-
-    """full filling the curve under the UV-Index curve"""
-    plt.fill_between(x, d_bts1day["uvind"],  where=d_bts1day["uvind"]<2, 
-                        facecolor='green', alpha=0.5, interpolate=True)
-    plt.fill_between(x, d_bts1day["uvind"],  where=d_bts1day["uvind"] >2 , 
-                        facecolor='yellow', alpha=0.5, interpolate=True)
-    plt.fill_between(x, d_bts1day["uvind"],  where=d_bts1day["uvind"] >5 , 
-                        facecolor='orange', alpha=0.5, interpolate=True)
-    plt.fill_between(x, d_bts1day["uvind"],  where=d_bts1day["uvind"] >7 , 
-                        facecolor='red', alpha=0.5, interpolate=True)
-    plt.fill_between(x, d_bts1day["uvind"],  where=d_bts1day["uvind"]>10, 
-                        facecolor='purple', alpha=0.5, interpolate=True)
+    par1.axis["right"].line.set_color(p1.get_color())
+    par1.axis["right"].major_ticks.set_color(p1.get_color())
+    par1.axis["right"].major_ticklabels.set_color(p1.get_color())
     
-    """adding footnote"""
-    plt.annotate('Leibnitz Institut für \nTroposphärenforschung', xy= (0,-1), xycoords='figure fraction',
-                 xytext=(0, 25), textcoords='offset points', ha="left", va="bottom")
+    par2.axis["right"].line.set_color(p2.get_color())
+    par2.axis["right"].major_ticks.set_color(p2.get_color())
+    par2.axis["right"].major_ticklabels.set_color(p2.get_color())
 
-    """save the plot as pdf file """
-    plot_name = config.get('DEFAULT','image_path') + 'BTS_plot-' + day.strftime('%Y%m%d') + '.pdf'
-    plt.savefig(plot_name)
+
+    """Add chart plot UV index"""
+    # Get a color map
+    my_cmap = cm.get_cmap('RdYlGn',10)
+    # reverse
+    my_cmap_r = my_cmap.reversed()
+     
+    # Get normalize function (takes data in range [vmin, vmax] -> [0, 1])
+    my_norm = Normalize(vmin=0, vmax=10)
+    time_step=1/30 # in hours
+    plt.bar(x_dict["hour_decimal"], d_bts1day["uvind"], color=my_cmap_r(my_norm(d_bts1day["uvind"])), edgecolor='none', width=time_step)
+    
+    """Add colorbar"""
+    # https://stackoverflow.com/questions/51204505/python-barplot-with-colorbar
+    sm = ScalarMappable(cmap=my_cmap_r, norm=plt.Normalize(0,12))
+    sm.set_array([])
+
+    # https://stackoverflow.com/questions/32462881/add-colorbar-to-existing-axis
+    cax = fig.add_axes([0.125, 0.11, 0.01, 0.77])
+    
+    cbar = plt.colorbar(sm, cax=cax, ticks=[])
+    #cbar.set_label('UV index', rotation=90,labelpad=5)
+    cbar.ax.tick_params(labelsize=7)
+    # END # colornar
+    
+
+    """save the plot as file """
+    plot_name = config.get('DEFAULT','image_path') + config.get('STATION','station_prefix') + '_' + day.strftime('%Y%m%d') + '_plot.png'
+    plt.savefig(plot_name, dpi=300)
+    plt.close()
     print("%-21s: %-60s" %('Plot data to file', plot_name))
