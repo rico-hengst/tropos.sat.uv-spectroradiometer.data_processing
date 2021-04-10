39312dacc097a0f0a1335a905b9959fa73074018
Your Name <you@example.com>
2021-04-10 17:49:59 +0200

    Improve setup file, now packaging include configs

--- setup.py
+++ setup.py
@@ -11,14 +11,17 @@ setup(
     description     = "The software package contains scripts to process UV radiation data, based on measurements with the spectroradiometer BTS2048. The Python software package is tailored to read the data in the manufacturer data format 'Solarscan'. The software package is able to export the data in the netcdf format and to generate plots of the measured and dervied variables.",
     packages        = [ 'tropos_uv' ],
     package_dir     = {'tropos_uv': 'src'}, # Our packages live under src but src is not a package itself
-    #packages= setuptools.find_packages(where="src"),
+    package_data    = { # include config files
+        # If any package contains * files, include them:
+        "": ["config/templates/*"],
+    },
     python_requires = '>=3.7',
     install_requires=[
         'argparse',
     ],
     entry_points    = { 
         'console_scripts': [ 
-            'troposuv = tropos_uv.bts_process.run' 
+            'troposuv = tropos_uv.bts_process:run' 
         ] 
     },  
      classifiers    = [
