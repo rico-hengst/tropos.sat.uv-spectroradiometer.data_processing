867c026a45c8c8b063665599fa025ccea20867eb
Your Name <you@example.com>
2021-04-10 17:07:46 +0200

    Improve setup

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
-import setuptools
+from setuptools import setup
 
 #with open("README.md", "r", encoding="utf-8") as fh:
     #long_description = fh.read()
 
-setuptools.setup(
-    name="troposzz", 
-    version="1.1",
-    author="Rico Hengst, Nicolas Bayer, Lionel Doppler",
-    author_email="rico.hengst@tropos.de",
-    description="The software package contains scripts to process UV radiation data, based on measurements with the spectroradiometer BTS2048. The Python software package is tailored to read the data in the manufacturer data format 'Solarscan'. The software package is able to export the data in the netcdf format and to generate plots of the measured and dervied variables.",
-    package_dir = {'': 'src'}, # Our packages live under src but src is not a package itself
-    packages=setuptools.find_packages(where="src"),
+setup(
+    name            = "tropos_uv", 
+    version         = "1.1",
+    author          = "Rico Hengst, Nicolas Bayer, Lionel Doppler",
+    author_email    = "rico.hengst@tropos.de",
+    description     = "The software package contains scripts to process UV radiation data, based on measurements with the spectroradiometer BTS2048. The Python software package is tailored to read the data in the manufacturer data format 'Solarscan'. The software package is able to export the data in the netcdf format and to generate plots of the measured and dervied variables.",
+    packages        = [ 'tropos_uv' ],
+    package_dir     = {'tropos_uv': 'src'}, # Our packages live under src but src is not a package itself
+    #packages= setuptools.find_packages(where="src"),
+    python_requires = '>=3.7',
     install_requires=[
         'argparse',
     ],
-    entry_points={ 
+    entry_points    = { 
         'console_scripts': [ 
-            'troposzz = src.troposzz.bts_process.__main__' 
+            'troposuv = tropos_uv.bts_process.run' 
         ] 
     },  
-     classifiers=[
+     classifiers    = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU GPL v3.0",
         "Operating System :: OS Independent",
